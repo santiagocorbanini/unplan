76a2370f1dff1330ebe71c51ac30f3ab455d7cc7
Santiago Corbanini <santiagocorbanini@gmail.com>
2025-01-06 08:37:38 -0300

    cambio api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true, // Asegura el envío de cookies entre cliente y servidor
 });
 
 export const fetchEventos = async () => {
