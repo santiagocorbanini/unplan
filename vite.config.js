9cb017e2c973a68756c81283c129477495607e4a
Santiago Corbanini <santiagocorbanini@gmail.com>
2025-01-13 11:52:18 -0300

    cambio vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "/unplan/",
+  server: {
+    proxy: {
+      "/shows": {
+        target: "https://149.50.146.27:3127", // La URL de tu API
+        changeOrigin: true,
+        secure: false, // Si el servidor tiene un certificado SSL auto-firmado
+      },
+    },
+  },
 });
